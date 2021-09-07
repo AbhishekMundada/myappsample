645202445b05133eba22bea455faf9ccd617ab3a
Abhishek Mundada <abhi.mundada29@gmail.com>
2021-09-07 16:26:58 +0530

    Changed new.cpp

---
+++ new.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=5, b=6;
+    float pi=3.14;
+    char ch='u';
+    bool is_true=true;
+    cout<<"This is tutorial 2.\nHere value of a is "<<a<<".\nAnd value of b is "<<b;
+    cout<<"\nValue of pi is "<<pi;
+    cout<<"\nValue of ch is "<<ch<<is_true;
+
+    return 0;
+}
